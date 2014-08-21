9e05ccef0adcc0b5fcfddeab3259612f699920de
qar0711 <joseqar@gmail.com>
2014-08-21 17:21:09 -0500

    Poetaman es un flojo

---
+++ Proyecto/Aplicacion/Controllers/PersonasController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BDts;
+
+namespace Aplicacion.Controllers
+{
+    public class PersonasController : Controller
+    {
+        private Bd_GYMEntities db = new Bd_GYMEntities();
+
+        //
+        // GET: /Personas/
+
+        public ActionResult Index()
+        {
+            return View(db.Personas.ToList());
+        }
+
+        //
+        // GET: /Personas/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Persona persona = db.Personas.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        //
+        // GET: /Personas/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Personas/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Personas.Add(persona);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(persona);
+        }
+
+        //
+        // GET: /Personas/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Persona persona = db.Personas.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        //
+        // POST: /Personas/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(persona).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(persona);
+        }
+
+        //
+        // GET: /Personas/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Persona persona = db.Personas.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        //
+        // POST: /Personas/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Persona persona = db.Personas.Find(id);
+            db.Personas.Remove(persona);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Proyecto/Aplicacion/Models/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Aplicacion.Models
+{
+    public class Program
+    {
+        //Poetaman es un flojo q no pudo con eso
+        public int MyProperty { get; set; }
+    }
+}
